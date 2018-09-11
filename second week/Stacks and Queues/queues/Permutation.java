7999cf8f63735891ab3df21b849f730ecb7fd7d8
huipengly <huipengly@gmail.com>
2018-09-11 09:32:31 +0800

    通过所有测试，包括extra

--- second week/Stacks and Queues/queues/Permutation.java
+++ second week/Stacks and Queues/queues/Permutation.java
@@ -7,17 +7,30 @@
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
     public static void main(String[] args) {
         RandomizedQueue<String> sQueue = new RandomizedQueue<String>();
-        int out_number = Integer.parseInt(args[0]);
+        int k = Integer.parseInt(args[0]);
+        if (k == 0)
+            return;             // 不需要输出的时候直接返回
+        int counter = k;
+        for (int i = 0; i != k; ++i) {
+            if (!StdIn.isEmpty()) {
+                String str = StdIn.readString();
+                sQueue.enqueue(str);
+            }
+        }
         while (!StdIn.isEmpty()) {
             String str = StdIn.readString();
-            sQueue.enqueue(str);
-            // StdOut.print(str);
+            ++counter;
+            if (StdRandom.uniform(counter) < k) {
+                sQueue.dequeue();
+                sQueue.enqueue(str);
+            }
         }
-        for (int i = 0; i != out_number; ++i) {
+        for (int i = 0; i != k; ++i) {
             StdOut.print(sQueue.dequeue() + "\n");
         }
     }
