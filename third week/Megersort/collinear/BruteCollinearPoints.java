ab3bd78c61859b6e91d78c86413f0994c739298a
huipengly <huipengly@gmail.com>
2018-09-18 09:21:36 +0800

    完成暴力搜索方法

---
+++ third week/Megersort/collinear/BruteCollinearPoints.java
@@ -0,0 +1,73 @@
+/* *****************************************************************************
+ *  Name: 暴力判断是否共线
+ *  Date: 20180917
+ *  Description:
+ *  author: huipengly
+ **************************************************************************** */
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BruteCollinearPoints {
+    Point[] points;
+    int number = 0;
+    LineSegment[] lineSegments = new LineSegment[3];
+
+    public BruteCollinearPoints(Point[] points) {    // finds all line segments containing 4 points
+        if (points == null)
+            throw new java.lang.IllegalArgumentException("argument to the constructor is null.");
+        for (int i = 0; i != points.length; ++i) {
+            if (points[i] == null)
+                throw new java.lang.IllegalArgumentException("include null Point.");
+        }
+        Arrays.sort(points);
+        for (int i = 0; i != points.length - 1; ++i) {
+            if (points[i].equals(points[i + 1]))
+                throw new java.lang.IllegalArgumentException("include same Point.");
+        }
+        this.points = points;
+        findLine();
+    }
+
+    public void findLine() {    // 寻找连成线的四个点
+        for (int i = 0; i != points.length - 3; ++i) {
+            for (int j = i + 1; j != points.length - 2; ++j) {
+                for (int k = j + 1; k != points.length - 1; ++k) {
+                    Comparator<Point> comparator = points[i]
+                            .slopeOrder();      // 与point[i]点连线的斜率作比较
+                    if (comparator.compare(points[j], points[k]) == 0) {    // 斜率相同说明此i,j,k共线，寻找下一个l
+                        lineSegments[0] = new LineSegment(points[i], points[j]);
+                        lineSegments[1] = new LineSegment(points[j], points[k]);
+                        number = 2;
+                        for (int i1 = k + 1; i1 != points.length; ++i1) {
+                            if (comparator.compare(points[k], points[i1]) == 0) {
+                                lineSegments[2] = new LineSegment(points[k], points[i1]);
+                                ++number;
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public int numberOfSegments() {        // the number of line segments
+        return number;
+    }
+
+    public LineSegment[] segments() {               // the line segments
+        return lineSegments;
+    }
+
+    public static void main(String[] args) {
+        Point[] p = new Point[6];
+        p[0] = new Point(19000, 10000);
+        p[1] = new Point(18000, 10000);
+        p[2] = new Point(32000, 10000);
+        p[3] = new Point(21000, 10000);
+        p[4] = new Point(1234, 5678);
+        p[5] = new Point(14000, 10000);
+        BruteCollinearPoints bcp = new BruteCollinearPoints(p);
+    }
+}
