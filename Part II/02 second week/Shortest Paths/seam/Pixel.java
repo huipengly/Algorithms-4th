acc4e7c0970b66fa9972e4ec3e4fe37c11ea5751
huipengly <huipengly@gmail.com>
2018-10-23 20:10:31 +0800

    添加一个像素类

---
+++ Part II/02 second week/Shortest Paths/seam/Pixel.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Pixel {
+    private int x, y;
+
+    public Pixel(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
