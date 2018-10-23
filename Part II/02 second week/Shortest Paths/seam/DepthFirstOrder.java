a07cf141f1c9829560552d41ca0b054039d80aee
huipengly <huipengly@gmail.com>
2018-10-23 20:11:42 +0800

    使用新的Pixel表示点，修改行列在二维数组中的表示

--- Part II/02 second week/Shortest Paths/seam/DepthFirstOrder.java
+++ Part II/02 second week/Shortest Paths/seam/DepthFirstOrder.java
@@ -8,18 +8,18 @@ import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
 
 public class DepthFirstOrder {
-    private int[][] p;
+    private double[][] p;
     private boolean[][] marked;
     private int width;
     private int height;
-    private Stack<Point> reversePost = new Stack<>();
+    private Stack<Pixel> reversePost = new Stack<>();
 
     // p是一个二维像素矩阵，代表矩阵能量
-    public DepthFirstOrder(int[][] p) {
+    public DepthFirstOrder(double[][] p) {
         this.p = p;
-        width = p[0].length;
-        height = p.length;
-        marked = new boolean[height][width];
+        width = p.length;
+        height = p[0].length;
+        marked = new boolean[width][height];
         for (boolean[] m : marked) {
             for (boolean mm : m) {
                 mm = false;
@@ -27,48 +27,38 @@ public class DepthFirstOrder {
         }
         // 这里不认为第一行的点特殊，认为有一个虚拟起点。第一个for是要遍历第一行
         for (int i = 0; i != width; ++i)
-            if (!marked[0][i]) dfs(0, i);
+            if (!marked[i][0]) dfs(i, 0);
     }
 
-    Iterable<Point> reversePost() {
+    Iterable<Pixel> reversePost() {
         return reversePost;
-    }
-
-    private class Point {
-        private int x;
-        private int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 
     private void dfs(int x, int y) {
         marked[x][y] = true;
         // 判断是否在最后一行了
-        if (x != height - 1) {
+        if (y != height - 1) {
             // 左下
-            if (y != 0 && !marked[x + 1][y - 1]) {
-                dfs(x + 1, y - 1);
+            if (x != 0 && !marked[x - 1][y + 1]) {
+                dfs(x - 1, y + 1);
             }
             // 下方
-            if (!marked[x + 1][y]) {
-                dfs(x + 1, y);
+            if (!marked[x][y + 1]) {
+                dfs(x, y + 1);
             }
             // 右下
-            if (y != width - 1 && !marked[x + 1][y + 1]) {
+            if (x != width - 1 && !marked[x + 1][y + 1]) {
                 dfs(x + 1, y + 1);
             }
         }
-        reversePost.push(new Point(x, y));
+        reversePost.push(new Pixel(x, y));
     }
 
     public static void main(String[] args) {
-        int[][] test = new int[3][3];
+        double[][] test = new double[3][3];
         DepthFirstOrder dfs = new DepthFirstOrder(test);
-        for (Point p : dfs.reversePost()) {
-            StdOut.print(p.x + " " + p.y + "\n");
+        for (Pixel p : dfs.reversePost()) {
+            StdOut.print(p.x() + " " + p.y() + "\n");
         }
     }
 }
