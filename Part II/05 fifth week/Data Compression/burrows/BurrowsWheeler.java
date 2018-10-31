4be24a02f2c58274deae0642f8f8a3b67e16f3b8
huipengly <huipengly@gmail.com>
2018-10-31 10:34:21 +0800

    删除部分没用的注释，修正key-index方法程序

--- Part II/05 fifth week/Data Compression/burrows/BurrowsWheeler.java
+++ Part II/05 fifth week/Data Compression/burrows/BurrowsWheeler.java
@@ -11,14 +11,7 @@ import edu.princeton.cs.algs4.BinaryStdOut;
 public class BurrowsWheeler {
     // apply Burrows-Wheeler transform, reading from standard input and writing to standard output
     public static void transform() {
-        char startFlag = 0;     // 起始位
-        StringBuilder originalStringBuilder = new StringBuilder();
         // 从流中构造原始字符串
-        // while (!BinaryStdIn.isEmpty()) {
-        //     char c = BinaryStdIn.readChar();
-        //     originalStringBuilder.append(c);
-        // }
-        // String originalString = originalStringBuilder.toString();
         String originalString = BinaryStdIn.readString();
         // StdOut.print(originalString + "\n");
         CircularSuffixArray csa = new CircularSuffixArray(originalString);
@@ -32,7 +25,6 @@ public class BurrowsWheeler {
         //     StdOut.print(csa.index(i) + "\t");
         // }
         for (int i = 0; i != csa.length(); ++i) {   // 写入数据
-            // BinaryStdOut.write(originalString.charAt(csa.index(i)));
             int index = csa.index(i);
             // 取index的最后一个字母，既index前一个。如果index为0，则取最后一个
             int endCharIndex = index == 0 ? (csa.length() - 1) : (index - 1);
@@ -54,8 +46,8 @@ public class BurrowsWheeler {
         for (int i = 0; i != length; ++i) {
             ++count[t.charAt(i) + 1];
         }
-        for (int i = 1; i != length; ++i) {
-            count[i] += count[i - 1];
+        for (int i = 0; i != R; ++i) {
+            count[i + 1] += count[i];
         }
         for (int i = 0; i != length; ++i) {
             char c = t.charAt(i);
@@ -71,7 +63,6 @@ public class BurrowsWheeler {
     // 假设输入流一定正确，不需要检测
     public static void inverseTransform() {
         int first;
-        StringBuilder transformedStringBuilder = new StringBuilder();
         // 从流中构造原始字符串
         // 读取first
         if (!BinaryStdIn.isEmpty()) {
@@ -82,12 +73,6 @@ public class BurrowsWheeler {
         }
         // 读取编码
         String t = BinaryStdIn.readString();
-        // while (!BinaryStdIn.isEmpty()) {
-        //     char c = BinaryStdIn.readChar();
-        //     transformedStringBuilder.append(c);
-        // }
-        // String t = transformedStringBuilder.toString();
-        // char[] originalChar = new char[t.length()];
         int[] next = constructeNext(t);
         // int[] next = { 3, 0, 6, 7, 8, 9, 10, 11, 5, 2, 1, 4 };
 
